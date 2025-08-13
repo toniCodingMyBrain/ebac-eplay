bf947b9660c26ebf5be36ca30237ddef620895f2
toni500madmax <toni500madmax@gmail.com>
2025-08-13 09:20:41 -0300

    replace: new api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -38,7 +38,7 @@ type PurchasePayload = {
 
 const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://fake-api-tau.vercel.app/api/eplay'
+    baseUrl: 'https://ebac-fake-api.vercel.app/api/eplay/'
   }),
   endpoints: (builder) => ({
     getFeaturedGames: builder.query<Game, void>({
